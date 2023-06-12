ee89b3ca45645d81f9a3758d6e6c3323a0a445ef
Momen Qudaih <momenqudaih1@gmail.com>
2023-06-12 11:20:45 +0300

    feat: init commit2

--- server/models/roles.ts
+++ server/models/roles.ts
@@ -1,18 +1,18 @@
-// I need the connection file here I am only gonna assume that momen export it as sequelize
-import { DataTypes } from 'sequelize';
-import { RolesAttributes } from '../utils/types/roles';
-import sequelize from './connection';
+// // I need the connection file here I am only gonna assume that momen export it as sequelize
+// import { DataTypes } from 'sequelize';
+// import { RolesAttributes } from '../utils/types/roles';
+// import sequelize from './connection';
 
-const Roles = sequelize.define<RolesAttributes>('roles', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  role: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+// const Roles = sequelize.define<RolesAttributes>('roles', {
+//   id: {
+//     type: DataTypes.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//   },
+//   role: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+// });
 
-export default Roles;
+// export default Roles;
