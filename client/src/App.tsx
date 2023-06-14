aabd9250cc6757fb607688eb1478d93a35cab94c
Momen Qudaih <momenqudaih1@gmail.com>
2023-06-14 09:56:21 +0300

    feat: login router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
-/* eslint-disable react/button-has-type */
-import react from 'react';
-import './App.css';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+// eslint-disable-next-line import/extensions
+import Login from './pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Home</h1>,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
 
 const App = () => {
-  return <>Hello</>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
