465e7034a1ea5e278b040c90ec3a579c971865a9
momenmarwan <momenalgaga82@gmail.com>
2023-06-18 16:05:11 +0300

    ci: request changes

--- server/controllers/auth/login.ts
+++ server/controllers/auth/login.ts
@@ -4,6 +4,9 @@ import { loginSchema } from '../../utils';
 import { CustomError } from '../../utils';
 import { StatusCodes } from '../../utils/enum';
 import { comparePassword } from '../../utils';
+import { signToken } from '../../utils';
+import { Role } from '../../models';
+import { staffAttribute } from '../../utils/types';
 
 const loginController = async (
   req: Request,
@@ -13,7 +16,16 @@ const loginController = async (
   try {
     const { username, password } = await loginSchema.validateAsync(req.body);
 
-    const existStaff = await Staff.findOne({ where: { username } });
+    const existStaff = (await Staff.findOne({
+      where: { username },
+      include: [
+        {
+          model: Role,
+          attributes: ['role'],
+        },
+      ],
+    })) as staffAttribute & { Role: { role: string } };
+
     if (!existStaff) {
       throw new CustomError(
         StatusCodes.Unauthenticated,
@@ -22,21 +34,25 @@ const loginController = async (
     }
 
     const isMatch = await comparePassword(password, existStaff.password);
-    if (isMatch) {
-      res.json({
-        error: false,
-        msg: 'the has been login successfully',
-        data: {
-          id: existStaff.id,
-          username: existStaff.username,
-        },
-      });
-    } else {
+    const token = await signToken({
+      id: existStaff.id as number,
+      username,
+      role: existStaff.Role.role,
+    });
+    if (!isMatch) {
       throw new CustomError(
         StatusCodes.Unauthenticated,
         'please check the password',
       );
     }
+    res.cookie('token', token).json({
+      error: false,
+      msg: 'the has been login successfully',
+      data: {
+        id: existStaff.id,
+        username: existStaff.username,
+      },
+    });
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     if (error.name === 'ValidationError') {
