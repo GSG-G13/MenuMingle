d161213d42196cb999bf99a066c41dc4ac93a641
Momen Qudaih <momenqudaih1@gmail.com>
2023-07-04 12:20:12 +0300

    fix: solve conflicts

--- client/src/components/watingRoomComponent/Picture.tsx
+++ client/src/components/watingRoomComponent/Picture.tsx
@@ -1,10 +1,7 @@
 import inProgress from '../../assets/Group 17388.png';
 
-<<<<<<< HEAD
 const Picture = (status: any) => {
-=======
 const Picture = () => {
->>>>>>> 7e2e4d00d5835b1ad85b2c58fddccdcaed0266ec
   return (
     <img
       src={inProgress}
